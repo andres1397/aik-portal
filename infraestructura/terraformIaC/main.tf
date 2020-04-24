d76bc56939b78399cbfb4b81b901e674f232a365
ManuelCoral1998 <almacolo23@hotmail.com>
2020-04-24 02:25:17 -0500

    cambios finales en el main.tf

--- infraestructura/terraformIaC/main.tf
+++ infraestructura/terraformIaC/main.tf
@@ -316,15 +316,17 @@ resource "aws_instance" "aik-portal-back" {
         sudo yum update -y
         sudo yum install -y git 
         # Clonar nuestro repositorio 
-        git clone https://github.com/andres1397/aik-portal /srv/aik-portal
+        sudo git clone -b Feature-FrontBackInfra-ImplementacionDiseñoAWS https://github.com/andres1397/aik-portal /srv/aik-portal
         
         # Instalar SaltStack
-        sudo yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm
-        sudo yum clean expire-cache;sudo yum -y install salt-minion; chkconfig salt-minion off
-        
+        sudo curl -L https://bootstrap.saltstack.com -o bootstrap_salt.sh
+        sudo sh bootstrap_salt.sh
+        #sudo yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm
+        #sudo yum clean expire-cache;sudo yum -y install salt-minion; chkconfig salt-minion off
+
         #Put custom minion config in place (for enabling masterless mode)
         sudo cp -r /srv/aik-portal/Configuration_management/minion.d /etc/salt/
-        echo -e 'grains:\n roles:\n  - backend' > /etc/salt/minion.d/grains.conf
+        echo -e 'grains:\n roles:\n  - backend' | sudo tee /etc/salt/minion.d/grains.conf
         
         # Realizar un saltstack completo
         sudo salt-call state.apply
