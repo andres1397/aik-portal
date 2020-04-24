aaa6596b89556c45d26c7eae303eec33abf1f747
andres1397 <andres.aguirre1397@gmail.com>
2020-04-24 00:38:52 -0700

    Two Hotfix and IP solution more info in slack

--- aik-app-ui/server.js
+++ aik-app-ui/server.js
@@ -3,9 +3,44 @@
 // Declare our dependencies
 var express = require('express');
 var request = require('superagent');
-DIR_IP = "1.1.1.1";
-var ip = DIR_IP;
-var localhost = process.env.BACK_HOST || ip;
+let localIP;
+var os = require('os');
+var ifaces = os.networkInterfaces();
+Object.keys(ifaces).forEach(function (ifname) {
+   var alias = 0;
+
+   ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+         // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+         return;
+      }
+
+      if(ifname === 'ens33') {
+         if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+         } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+         }
+         ++alias;
+         localIP = iface.address;
+      }
+      else{
+        if (alias >= 1) {
+          // this single interface has multiple ipv4 addresses
+          console.log(ifname + ':' + alias, iface.address);
+       } else {
+          // this interface has only one ipv4 adress
+          console.log(ifname, iface.address);
+       }
+       ++alias;
+       localIP = iface.address;
+
+      }
+   });
+});
+var localhost = process.env.BACK_HOST || localIP;
 // Create our express app
 var app = express();
 
