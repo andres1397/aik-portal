072f5f5fce7d7ce02ee9b45742d612ac6d2450a0
ManuelCoral1998 <almacolo23@hotmail.com>
2020-05-07 01:22:13 -0500

    outputs.tf

--- infraestructura/terraformIaC/outputs.tf
+++ infraestructura/terraformIaC/outputs.tf
@@ -1,4 +1,8 @@
  
 output "IP-PORTAL" {
-  value = "${aws_instance.aik-portal-front.public_ip}"
+  value = aws_instance.aik-portal-front[0].public_ip
+}
+
+output "DB" {
+  value = aws_db_instance.My-SQL-Database[0].address
 }
